66a80dbb86af6a59d2a84c31ce02c45dfca40a7d
Robert Banagale <rob@banagale.com>
2021-03-07 18:07:43 -0800

    Do not update the issue, update the issue comment

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function run() {
   try {
     const githubToken = core.getInput("GITHUB_TOKEN");
     const octokit = new Octokit({ auth: githubToken });
-    const customMessage = core.getInput("message");
     const { context } = github;
     const repository = context.payload.repository;
 
@@ -16,21 +15,23 @@ function run() {
       if (context.payload.action === "created" || context.payload.action === "edited") {
         const issueNumber = context.payload.issue.number;
 
-        const comment = context.payload.comment.body;
+        const commentBody = context.payload.comment.body;
+        const commentId = context.payload.comment.id;
         const commentObject = context.payload.comment;
 
-        const filteredComment = filterEmailChain(comment);
+        const filteredComment = filterEmailChain(commentBody);
         console.log(filteredComment);
 
         const commentAuthor = commentObject.user.login;
-        const message = customMessage ? customMessage : `Hey @${commentAuthor}! 👋 <br/> You're great 😔</br>🙂`;
-        console.log('this is a test of console logging from an action 👋');
+        const message = `Hey @${commentAuthor}! 👋 <br/> You're great 😔</br>🙂`;
 
-        const ignore = octokit.issues.update({
+        console.log('owner', repository.owner, 'issue number: ', issueNumber, 'issue comment id: ', commentId);
+
+        const ignore = octokit.issues.updateComment({
           owner: repository.owner.login,
           repo: repository.name,
-          issue_number: issueNumber,
-          body: comment + message,
+          comment_id: commentId,
+          body: filteredComment + message,
         });
       }
     }

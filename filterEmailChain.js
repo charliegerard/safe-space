596b62442df51fcb42f842eb37ebe0d5c7c0ff07
Robert Banagale <rob@banagale.com>
2021-03-08 10:18:25 -0800

    Debug match logic, await result of filter

--- filterEmailChain.js
+++ filterEmailChain.js
@@ -2,10 +2,14 @@ module.exports = async function filterEmailChain(issueComment) {
   if (typeof issueComment != 'string') {
     throw new TypeError('filterEmailChain expects a string');
   }
-  const OUTLOOK_EMAIL_REGEX = /^\\s*(From:.*[^]*)\\b(?:unsubscribe.)$+/g;
+  const OUTLOOK_EMAIL_REGEX = /\^\\s\*\(From:\.\*\[\^]\*\)\\b\(\?:unsubscribe.\)\$\+/g;
   if (issueComment.match(OUTLOOK_EMAIL_REGEX) !== null) {
     console.log('Filtering issue comment for email remenants');
-    return issueComment.replace(OUTLOOK_EMAIL_REGEX, '');
+    console.log(issueComment.match(OUTLOOK_EMAIL_REGEX));
+    const filteredComment = async () => {
+      issueComment.replace(OUTLOOK_EMAIL_REGEX, '');
+    };
+    return await filteredComment();
   }
   return issueComment;
 };

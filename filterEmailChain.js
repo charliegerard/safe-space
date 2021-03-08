27cab0f189bb9c614c65f41cb6dd2fa230c56826
Robert Banagale <rob@banagale.com>
2021-03-08 15:11:48 -0800

    Debug regex

--- filterEmailChain.js
+++ filterEmailChain.js
@@ -2,7 +2,8 @@ module.exports = async function filterEmailChain(issueComment) {
   if (typeof issueComment != 'string') {
     throw new TypeError('filterEmailChain expects a string');
   }
-  const OUTLOOK_EMAIL_REGEX = /^\s*(From:.*[^]*)\b(?:unsubscribe.)$/g;
+  const OUTLOOK_EMAIL_REGEX = /^\s*(From:.*[^]*)\b(?:unsubscribe.)$/gm;
+  // const OUTLOOK_EMAIL_REGEX = /.*/g;
   if (issueComment.match(OUTLOOK_EMAIL_REGEX) !== null) {
     console.log('Filtering issue comment for email remenants');
     console.log(issueComment.match(OUTLOOK_EMAIL_REGEX));
@@ -10,6 +11,12 @@ module.exports = async function filterEmailChain(issueComment) {
       issueComment.replace(OUTLOOK_EMAIL_REGEX, '');
     };
     return await filteredComment();
+  } else {
+    console.log('No email reply detected');
+    console.log(issueComment.match(OUTLOOK_EMAIL_REGEX));
+    console.log('--------------------');
+    console.log(issueComment);
+    return false;
   }
-  return false;
+
 };

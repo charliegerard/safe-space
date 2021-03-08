d0ef22c4ac271c80f4733c5ae3ec5bca06972c0e
Robert Banagale <rob@banagale.com>
2021-03-08 15:08:43 -0800

    Perhaps escaping is unncessary

--- filterEmailChain.js
+++ filterEmailChain.js
@@ -2,7 +2,7 @@ module.exports = async function filterEmailChain(issueComment) {
   if (typeof issueComment != 'string') {
     throw new TypeError('filterEmailChain expects a string');
   }
-  const OUTLOOK_EMAIL_REGEX = /\^\\s\*\(From:\.\*\[\^]\*\)\\b\(\?:unsubscribe.\)\$\+/g;
+  const OUTLOOK_EMAIL_REGEX = /^\s*(From:.*[^]*)\b(?:unsubscribe.)$/g;
   if (issueComment.match(OUTLOOK_EMAIL_REGEX) !== null) {
     console.log('Filtering issue comment for email remenants');
     console.log(issueComment.match(OUTLOOK_EMAIL_REGEX));

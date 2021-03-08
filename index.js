f5813b85ff36900d77c609104ded0e05608e9b17
Robert Banagale <rob@banagale.com>
2021-03-07 17:20:38 -0800

    Factor out tensorflow

--- index.js
+++ index.js
@@ -2,61 +2,35 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { Octokit } = require("@octokit/rest");
 
-async function run() {
+function run() {
   const filterEmailChain = require('./filterEmailChain');
-  const tf = require("@tensorflow/tfjs");
-  const toxicity = require("@tensorflow-models/toxicity");
-  await tf.setBackend("cpu");
 
   try {
     const githubToken = core.getInput("GITHUB_TOKEN");
     const octokit = new Octokit({ auth: githubToken });
     const customMessage = core.getInput("message");
-    const toxicityThreshold = core.getInput("toxicity_threshold");
     const { context } = github;
     const repository = context.payload.repository;
-    const threshold = toxicityThreshold ? toxicityThreshold : 0.9;
 
     if (context.payload.comment) {
-      if (
-        context.payload.action === "created" ||
-        context.payload.action === "edited"
-      ) {
+      if (context.payload.action === "created" || context.payload.action === "edited") {
         const issueNumber = context.payload.issue.number;
-        const model = await toxicity.load(threshold);
 
-        const comment = [context.payload.comment.body];
+        const comment = context.payload.comment.body;
         const commentObject = context.payload.comment;
 
-        const filteredComment = filterEmailChain(context.payload.comment.body);
+        const filteredComment = filterEmailChain(comment);
         console.log(filteredComment);
 
-        let toxicComment = undefined;
-        model.classify(comment).then((predictions) => {
-          predictions.forEach((prediction) => {
-            if (toxicComment) {
-              return;
-            }
-            prediction.results.forEach((result, index) => {
-              if (toxicComment) {
-                return;
-              }
-              if (result.match) {
-                const commentAuthor = commentObject.user.login;
-                toxicComment = commentObject.body;
-                const message = customMessage
-                  ? customMessage
-                  : `Hey @${commentAuthor}! 👋 <br/> You're great 😔</br>🙂`;
-                console.log('this is a test of console logging from an action 👋');
-                return octokit.issues.createComment({
-                  owner: repository.owner.login,
-                  repo: repository.name,
-                  issue_number: issueNumber,
-                  body: message,
-                });
-              }
-            });
-          });
+        const commentAuthor = commentObject.user.login;
+        const message = customMessage ? customMessage : `Hey @${commentAuthor}! 👋 <br/> You're great 😔</br>🙂`;
+        console.log('this is a test of console logging from an action 👋');
+
+        const ignore = octokit.issues.createComment({
+          owner: repository.owner.login,
+          repo: repository.name,
+          issue_number: issueNumber,
+          body: message,
         });
       }
     }

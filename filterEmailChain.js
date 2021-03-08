221d0dc1ad80f57df847bf247e2188cb00b4e038
Robert Banagale <rob@banagale.com>
2021-03-08 15:47:39 -0800

    Handle replies from gmail

--- filterEmailChain.js
+++ filterEmailChain.js
@@ -3,19 +3,18 @@ module.exports = async function filterEmailChain(issueComment) {
     throw new TypeError('filterEmailChain expects a string');
   }
   const OUTLOOK_EMAIL_REGEX = /^\s*(From:.*[^]*)\b(?:unsubscribe.)$/gm;
-  // const OUTLOOK_EMAIL_REGEX = /.*/g;
+  const GMAIL_EMAIL_REGEX = /^\s*(on .*[^]*)\b(wrote:.*[^]*)+(reply to this email directly,.*[^]*)+>(.*\s*)$/gm;
+
   if (issueComment.match(OUTLOOK_EMAIL_REGEX) !== null) {
     console.log('Filtering issue comment for email remenants');
-    console.log(issueComment.match(OUTLOOK_EMAIL_REGEX));
     const filteredComment = async () => {
       return issueComment.replace(OUTLOOK_EMAIL_REGEX, '');
     };
     return await filteredComment();
+  } else if (issueComment.match(GMAIL_EMAIL_REGEX) !== null) {
+    return issueComment.replace(GMAIL_EMAIL_REGEX, '');
   } else {
     console.log('No email reply detected');
-    console.log(issueComment.match(OUTLOOK_EMAIL_REGEX));
-    console.log('--------------------');
-    console.log(issueComment);
     return false;
   }
 

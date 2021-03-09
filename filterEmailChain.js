565881a3cd6693d60fccf7f128b578e17c05fd07
Robert Banagale <rob@banagale.com>
2021-03-08 16:29:49 -0800

    Debug return

--- filterEmailChain.js
+++ filterEmailChain.js
@@ -13,12 +13,13 @@ module.exports = async function filterEmailChain(issueComment) {
     const GMAIL_EMAIL_REGEX = /^\s*(on .*[^]*)\b(wrote:.*[^]*)+(reply to this email directly,.*[^]*)+>(.*\s*)$/gm;
 
     if (issueComment.match(OUTLOOK_EMAIL_REGEX) !== null) {
-      console.log('Filtering issue comment for email remenants');
+      console.log('Filtering issue comment for outlook remenants');
       const filteredComment = async () => {
         return issueComment.replace(OUTLOOK_EMAIL_REGEX, '');
       };
       return await filteredComment();
     } else if (issueComment.match(GMAIL_EMAIL_REGEX) !== null) {
+      console.log('Filtering issue comment for gmail remenants');
       const filteredComment = async () => {
         return issueComment.replace(GMAIL_EMAIL_REGEX, '');
       };
@@ -39,8 +40,10 @@ module.exports = async function filterEmailChain(issueComment) {
       return updatedComment.replace(BOSS_COPY_REGEX, '');
     }
   };
-
   const updatedComment = await filterReply(issueComment);
+
+  console.log('updatedComment is: ');
+  console.log(updatedComment);
 
   if (updatedComment !== false) {
     return await filterBoss(updatedComment);

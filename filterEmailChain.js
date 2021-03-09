b1565ee2cf889208f2ef74f3230244f40e90a4c7
Robert Banagale <rob@banagale.com>
2021-03-08 16:13:37 -0800

    If bossbot annotation is included in a new reply, remove previous annotation

--- filterEmailChain.js
+++ filterEmailChain.js
@@ -3,7 +3,12 @@ module.exports = async function filterEmailChain(issueComment) {
     throw new TypeError('filterEmailChain expects a string');
   }
 
-  const filterComment = async (issueComment) => {
+  const filterReply = async (issueComment) => {
+    /**
+     * Remove email chains from an issue comment
+     * Handles Outlook and Gmail
+     * @type {RegExp}
+     */
     const OUTLOOK_EMAIL_REGEX = /^\s*(From:.*[^]*)\b(?:unsubscribe.)$/gm;
     const GMAIL_EMAIL_REGEX = /^\s*(on .*[^]*)\b(wrote:.*[^]*)+(reply to this email directly,.*[^]*)+>(.*\s*)$/gm;
 
@@ -21,6 +26,22 @@ module.exports = async function filterEmailChain(issueComment) {
     }
   };
 
-  return await filterComment(issueComment);
+  const filterBoss = async (updatedComment) => {
+    /**
+     * Remove any previous bossBot annotations
+     * @type {RegExp}
+     */
+    const BOSS_COPY_REGEX = /^ *(<br\/><sub>🤖+.*🤖<\/sub>)\s*$/gm;
+    if ((updatedComment !== false) && (updatedComment.match(BOSS_COPY_REGEX) !== null)) {
+      return updatedComment.replace(BOSS_COPY_REGEX, '');
+    }
+  };
 
+  const updatedComment = filterReply(issueComment);
+
+  if (updatedComment !== false) {
+    return await filterBoss(updatedComment);
+  } else {
+    return await filterReply(issueComment);
+  }
 };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Octokit } = require("@octokit/rest");
 
 async function run() {
   const filterEmailChain = require('./filterEmailChain');
-
+  const BOSS_COPY = `<br/><sub>🤖 - Edited by BossBot - 🤖</sub>`;
   try {
     const githubToken = core.getInput("GITHUB_TOKEN");
     const octokit = new Octokit({ auth: githubToken });
@@ -19,10 +19,9 @@ async function run() {
         const commentId = context.payload.comment.id;
 
         const filteredComment = await filterEmailChain(commentBody);
-        const message = `<br/><sub>🤖 - Edited by BossBot - 🤖</sub>`;
 
         if (filteredComment !== false) {
-          const revisedMessage = filteredComment + message;
+          const revisedMessage = filteredComment + BOSS_COPY;
           console.log('Issue comment meta: ', 'Repo owner:', repository.owner.login, 'Issue number: ', issueNumber, 'Comment id: ', commentId);
 
           const ignore = octokit.issues.updateComment({
